bfac7b2ff578d31007b13fd1395a710b5a88b815
Abhay Joshi <105213625+abhayjoshi201@users.noreply.github.com>
2024-04-08 20:05:58 +0000

    data types

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 1343432
+let accountEmail = "abhayjoshi@gmail.com"
+var accountPassword = "123445"
+accountCity = "Jaipur"
+
+let accountState;
+
+
+// accountId =2 // not allowed
+accountEmail ="sadasd.cjdck"
+accountPassword = "235138"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basic/02_data.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+
+console.log("abhay")
+
+let name = "Abhay"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint 
+// string => ""
+// Boolean => true/false
+// null => standalone value to tell specific condition
+// undefined => not yet defined
+// symbol => to find uniqueness 
+
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.table([typeof null, typeof undefined, typeof name, typeof age, typeof isLoggedIn ])
